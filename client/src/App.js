ad679188d57aefbd4405af29184b75e5df90115f
oak45985 <oak45985@gmail.com>
2022-10-25 19:04:12 -0500

    updated app with correct apolloclient wrappers

--- client/src/App.js
+++ client/src/App.js
@@ -1,21 +1,35 @@
 import React from 'react';
+import { ApolloProvider, ApolloClient, InMemoryCache, createHttpLink } from "@apollo/client";
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import SearchBooks from './pages/SearchBooks';
 import SavedBooks from './pages/SavedBooks';
 import Navbar from './components/Navbar';
 
 function App() {
+
+  const httpLink = createHttpLink({
+    uri: '/graphql',
+
+  });
+
+  const client = new ApolloClient({
+    link: httpLink,
+    catche: new InMemoryCache(),
+  });
+
   return (
-    <Router>
-      <>
-        <Navbar />
-        <Switch>
-          <Route exact path='/' component={SearchBooks} />
-          <Route exact path='/saved' component={SavedBooks} />
-          <Route render={() => <h1 className='display-2'>Wrong page!</h1>} />
-        </Switch>
-      </>
-    </Router>
+    <ApolloClient client={client}>
+      <Router>
+        <>
+          <Navbar />
+          <Switch>
+            <Route exact path='/' component={SearchBooks} />
+            <Route exact path='/saved' component={SavedBooks} />
+            <Route render={() => <h1 className='display-2'>Wrong page!</h1>} />
+          </Switch>
+        </>
+      </Router>
+    </ApolloClient>
   );
 }
 
