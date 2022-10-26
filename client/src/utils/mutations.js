5f947d46113d2e3706bea70583b2b9119e53e555
oak45985 <oak45985@gmail.com>
2022-10-26 15:21:49 -0500

    got the saved book page to load

--- client/src/utils/mutations.js
+++ client/src/utils/mutations.js
@@ -42,13 +42,21 @@ export const SAVE_BOOK = gql `
     }
 `;
 
-// export const REMOVE_BOOK = gql `
-//     mutation removeBook($bookId: String!) {
-//         removeBook(bookId: $bookId) {
+export const REMOVE_BOOK = gql `
+    mutation removeBook($bookId: String!) {
+        removeBook(bookId: $bookId) {
+            _id
+            username
+            email
+            savedBooks {
+                bookId
+                authors
+                image
+                description
+                title
+                link
+            }
 
-//         }
-//     }
-// `
-// export const SAVE_BOOK = gql `
-//     mutation saveBook()
-// `
+        }
+    }
+`
