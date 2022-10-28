2b6913161dec45f1be3e1bee64bd98cd292a5c32
oak45985 <oak45985@gmail.com>
2022-10-27 21:35:58 -0500

    reset server

--- server/server.js
+++ server/server.js
@@ -6,7 +6,6 @@ const { typeDefs, resolvers } = require('./schemas');
 const { authMiddleware } = require('./utils/auth');
 
 // const routes = require('./routes');
-const app = express();
 
 const PORT = process.env.PORT || 3001;
 const server = new ApolloServer({
@@ -14,24 +13,31 @@ const server = new ApolloServer({
   resolvers,
   context: authMiddleware
 });
+const app = express();
 
-server.applyMiddleware({ app });
-
-app.use(express.urlencoded({ extended: true }));
+app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
-if (process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.join(__dirname, '../client/build')));
-}
+const startApolloServer = async (typeDefs, resolvers) => {
 
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, '../client/build/index.html'));
-});
+  await server.start();
 
+  server.applyMiddleware({ app });
 
-db.once('open', () => {
+  if (process.env.NODE_ENV === 'production') {
+    app.use(express.static(path.join(__dirname, '../client/build')));
+  }
+
+  app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, '../client/build/index.html'));
+  });
+
+  db.once('open', () => {
     app.listen(PORT, () => {
       console.log(`API server on PORT ${PORT}`);
       console.log(`Use GraphQl at http://localhost:${PORT}${server.graphqlPath}`);
     });
-});
+  });
+};
+
+startApolloServer(typeDefs, resolvers);
